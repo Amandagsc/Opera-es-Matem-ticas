9d59bc6956ef4948c286ec71eb5e998edd974cab
Amanda Silva <amanda.silva@rumosolucoes.com>
2022-06-22 09:35:50 -0300

    first commit

---
+++ Operacoes_Matematicas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operacoes_Matematicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operações Matemáticas!");
+        }
+    }
+}
